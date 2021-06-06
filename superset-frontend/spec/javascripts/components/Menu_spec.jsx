ed0a2cec55c89fe03580acc19e6488ba8b2498af
James Rudd <james.rudd@switchdin.com>
2021-06-07 09:03:00 +1000

    add switchdin logos

--- superset-frontend/spec/javascripts/components/Menu_spec.jsx
+++ superset-frontend/spec/javascripts/components/Menu_spec.jsx
@@ -71,7 +71,7 @@ const defaultProps = {
     ],
     brand: {
       path: '/superset/profile/admin/',
-      icon: '/static/assets/images/superset-logo-horiz.png',
+      icon: '/static/assets/images/switchdin-logo-horiz.png',
       alt: 'Superset',
       width: '126',
     },
